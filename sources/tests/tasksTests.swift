dd542a767b21185f30324fa00abc79446b953a1d
Alejandro Ramirez <alejandro.ramirez@poqstudio.com>
2018-10-13 14:36:16 +0100

    Blank project.

---
+++ sources/tests/tasksTests.swift
@@ -0,0 +1,21 @@
+
+import XCTest
+@testable import tasks
+
+class tasksTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+
+}
---
+++ sources/uitests/tasksUITests.swift
@@ -0,0 +1,17 @@
+
+import XCTest
+
+class tasksUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+    }
+
+}
